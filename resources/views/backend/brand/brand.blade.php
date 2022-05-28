8c1c11ffb6ee7b88d1a0f9009c9416f8c3f27db6
Dianastuti21 <dianastuti2101@gmail.com>
2022-05-28 12:25:51 +0700

    perbaikan dian2

--- resources/views/backend/brand/brand.blade.php
+++ resources/views/backend/brand/brand.blade.php
@@ -8,7 +8,7 @@
                     <div class="row column_title">
                         <div class="col-md-12">
                            <div class="page_title">
-                              <h2>Dashboard</h2>
+                              <h2>Brand</h2>
                            </div>
                         </div>
                     </div>
